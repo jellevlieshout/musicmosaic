95cdd905b1bf9bba56ee8be1f463da6c35e57ddd
Jelle van Lieshout <jellevlieshout@gmail.com>
2025-04-02 13:34:33 +0200

    added some untracked files

---
+++ app/protected/gameplay/page.tsx
@@ -0,0 +1,18 @@
+import { useGameplayStore } from "@/stores/hitsterModelStore";
+
+export default function GameplayPage() {
+    const { currentPlayers, currentPlayerId, currentPlaylist, currentSongId } = useGameplayStore();
+    return (
+        <div>
+            <h1>Gameplay</h1>
+            <div>
+                <h2>Current Players</h2>
+                <ul>
+                    {currentPlayers?.map((player: Player) => (
+                        <li key={player.id}>{player.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
