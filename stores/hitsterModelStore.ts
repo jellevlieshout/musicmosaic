a41edbb4ad1d29b2ca13048cd9e05417a9674e16
Joachim Olsson <joaco@kth.se>
2025-04-02 11:16:35 +0200

    continue implementing functions

--- stores/hitsterModelStore.ts
+++ stores/hitsterModelStore.ts
@@ -30,15 +30,55 @@ type GameplayState = {
     setPlaylist: (playlist: Song[]) => void
     seatPlayersInRandomOrder: (players: Player[]) => void
     playRandomNewSongFromCurrentPlaylist: () => void
-    revealSongDetails: () => void
-    playerWasRight: () => void
-    playerWasWrong: () => void
+    // revealSongDetails: () => void
+    // playerWasRight: () => void
+    // playerWasWrong: () => void
 }
 
-export const useGameplayStore = create<GameplayState>()(set, get) => ({
+export const useGameplayStore = create<GameplayState>((set, get) => ({
     currentPlayers: null,
     currentPlayerId: null,
     currentPlaylist: null,
     currentSongId: null,
-})
 
+    setPlaylist: (playlist: Song[]) => {
+        function addHasBeenPlayedField(song: Song): Song {
+            return { ...song, hasBeenPlayed: false }
+        }
+        const updatedPlaylist = playlist.map(addHasBeenPlayedField)
+        set({ currentPlaylist: updatedPlaylist })
+    },
+
+    seatPlayersInRandomOrder: (players: Player[]) => {
+        const shuffled = players
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value)
+        set({ currentPlayers: shuffled, currentPlayerId: shuffled[0]?.id ?? null })
+    },
+
+    playRandomNewSongFromCurrentPlaylist: () => {
+        const { currentPlaylist } = get()
+        if (!currentPlaylist) 
+            return
+        const unheardSongs = currentPlaylist.filter(song => !song.hasBeenPlayed)
+        if (unheardSongs.length === 0) {
+            console.log("We ran out of new songs!")
+            return
+        }
+
+        const randomSong = unheardSongs[Math.floor(Math.random() * unheardSongs.length)]
+        const updatedPlaylist = currentPlaylist.map(song =>
+            song.id === randomSong.id ? { ...song, hasBeenPlayed: true } : song
+        )
+
+        const { currentPlayers, currentPlayerId } = get()
+        const player = currentPlayers?.find(p => p.id === currentPlayerId)
+
+        console.log(`${player?.name}'s time to shine!`)
+        player?.deck.forEach(song => console.log(song.year, song.title))
+        console.log("Playing", randomSong.title)
+
+        set({ currentPlaylist: updatedPlaylist, currentSongId: Number(randomSong.id) })
+    }
+}))
