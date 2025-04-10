876515f91442aa5d6ba2a403109f817d1c430519
Emma <emraible@uw.edu>
2025-04-10 09:39:22 +0200

    fixed issue with font size

--- views/HomeView.tsx
+++ views/HomeView.tsx
@@ -6,7 +6,7 @@ export default async function HomeView() {
     return (
       <>
         <div className="items-center flex flex-col gap-5">
-          <h1 className="neon-tubes-styling text-8xl">Music Mosaic</h1>
+          <h1 className="neon-tubes-styling" style={{fontSize: '6rem'}}>Music Mosaic</h1>
           <h1 className="neon-tubes-styling text-3xl">Welcome</h1>
           <Button asChild size="sm" variant={"secondary"}>
             <Link href="/protected/new-game">New game</Link>
